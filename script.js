52cb67e09786a420dc3316e67db4ca40a09544b3
Nurullo <khuwaidullaev@gmail.com>
2021-03-08 23:38:15 +0500

    Adding Sticky Nav

--- script.js
+++ script.js
@@ -37,3 +37,100 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
+
+//    ----Button Scrolling----
+btnScrollTo.addEventListener("click", e => {
+  const s1coords = section1.getBoundingClientRect();
+  section1.scrollIntoView({ behavior: "smooth" });
+});
+
+//    ----Page Navigation----
+//Event Propagation
+document.querySelectorAll(".nav__link").forEach(function(el) {
+  el.addEventListener("click", function (e) {
+    e.preventDefault();
+    const id = this.getAttribute("href")
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" })
+  })
+});
+
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  //Matching strategy
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+})
+
+//    ----Tabbed Components----
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
+
+tabsContainer.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".operations__tab");
+
+  //Guard clause
+  if (!clicked) return;
+  //Remove active classes
+  tabs.forEach(t => t.classList.remove("operations__tab--active"));
+  tabsContent.forEach(c => c.classList.remove("operations__content--active"));
+  
+  //Activate tab
+  clicked.classList.add("operations__tab--active");
+  
+  //Activate content area
+  document.querySelector(`.operations__content--${clicked.dataset.tab}`)
+  .classList.add("operations__content--active")
+})
+
+//    ----Menu Fade Animation----
+const nav = document.querySelector(".nav");
+
+function handleHover(opacity) {
+  return function(e) {
+    if (e.target.classList.contains("nav__link")) {
+      const link = e.target;
+      const siblings = link.closest(".nav").querySelectorAll(".nav__link");
+      // const logo = link.closest(".nav").querySelector("img");
+      
+      siblings.forEach(el => {
+        if (el !== link) el.style.opacity = opacity;
+      });
+      // logo.style.opacity = opacity;
+    }
+  }
+}
+
+nav.addEventListener("mouseover", handleHover(0.5));
+nav.addEventListener("mouseout", handleHover(1));
+
+
+//    ----Sticky Navigation----
+
+// //Approach #1
+// const initialCoords = section1.getBoundingClientRect();
+
+// window.addEventListener("scroll", () => {
+//   if (window.scrollY > initialCoords.top) nav.classList.add("sticky");
+//   else nav.classList.remove("sticky");
+// })
+
+const header = document.querySelector(".header");
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) nav.classList.add("sticky");
+  else nav.classList.remove("sticky");
+}
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`
+});
+headerObserver.observe(header)
+
